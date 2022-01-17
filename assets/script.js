da861a610ca6c4d2793ace725de6b56b426ee010
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-01-17 10:16:56 -0500

    key drums working

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,34 @@
+const keys = document.querySelectorAll('.drumKit');
+
+//on button click add playing = play sound
 window.addEventListener('keydown' , playDrum);
 function playDrum(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if(!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    //link HTML to js
+    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!sound) return;
+    sound.currentTime = 0;
+    sound.play();
+    //add in class
+    const key = document.querySelector(`.drumKit[data-key="${e.keyCode}"]`);
     key.classList.add('playing');
 };
 
+
+//link HTML to js
+keys.forEach( key => key.addEventListener('transitionend' , removePlayingClass ) );
+//remove the playing class when it has began/started
+function removePlayingClass(e){
+    this.classList.remove('playing');
+}
+
+
+// Play music by mouse click
+keys.forEach( key => key.addEventListener('click' , mouseDrum ) );
+function mouseDrum(e){
+    const keyCode = this.getAttribute('data-key');
+	const sound = document.querySelector(`audio[data-key="${keyCode}"]`);
+	sound.currentTime = 0;
+	sound.play();
+	const drumClick = document.querySelector(`.key[data-key="${keyCode}"]`);
+    drumClick.classList.add('playing');
+}
