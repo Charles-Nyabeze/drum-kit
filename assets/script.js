bbe81180fae00a21724bfd1d647817fdc0fe57f5
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-01-17 11:15:51 -0500

    finalized

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 const keys = document.querySelectorAll('.drumKit');
-
+const stopMusic = document.querySelector('#stop')
 //on button click add playing = play sound
 window.addEventListener('keydown' , playDrum);
 function playDrum(e) {
@@ -10,25 +10,14 @@ function playDrum(e) {
     sound.play();
     //add in class
     const key = document.querySelector(`.drumKit[data-key="${e.keyCode}"]`);
-    key.classList.add('playing');
+    key.classList.add('hit');
 };
 
 
 //link HTML to js
-keys.forEach( key => key.addEventListener('transitionend' , removePlayingClass ) );
+keys.forEach( key => key.addEventListener('transitionend' , removeHit ) );
 //remove the playing class when it has began/started
-function removePlayingClass(e){
-    this.classList.remove('playing');
+function removeHit(e){
+    this.classList.remove('hit');
 }
 
-
-// Play music by mouse click
-keys.forEach( key => key.addEventListener('click' , mouseDrum ) );
-function mouseDrum(e){
-    const keyCode = this.getAttribute('data-key');
-	const sound = document.querySelector(`audio[data-key="${keyCode}"]`);
-	sound.currentTime = 0;
-	sound.play();
-	const drumClick = document.querySelector(`.key[data-key="${keyCode}"]`);
-    drumClick.classList.add('playing');
-}
